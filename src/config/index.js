8d346bf85c64b8190b2feae36d5cbe4861a48ec4
Abdan Zam Zam Ramadhan <abdan.zamzamramadhan@ai.astra.co.id>
2023-04-08 14:09:48 +0700

    Setup config sequelize

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+const { config } = require("dotenv");
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const CREDENTIALS = process.env.CREDENTIALS === "true";
+export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE, DB_DIALECT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
