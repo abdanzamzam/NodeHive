293ae3ce2c18c0bc76ca7c558b32bb50e898708a
Abdan Zam Zam Ramadhan <abdan.zamzamramadhan@ai.astra.co.id>
2023-04-09 08:30:07 +0700

    Fixing config sequelize

--- src/models/index.js
+++ src/models/index.js
@@ -4,29 +4,34 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-
-const { NODE_ENV, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE, DB_DIALECT } = require("../config");
+const config = require(__dirname + "/../config/database.js");
+const { NODE_ENV } = require("../config");
 
 const db = {};
 
-let sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-  dialect: DB_DIALECT,
-  host: DB_HOST,
-  port: DB_PORT,
-  timezone: "+07:00",
-  define: {
-    charset: "utf8mb4",
-    collate: "utf8mb4_general_ci",
-    underscored: true,
-    freezeTableName: true,
-  },
-  pool: {
-    min: 0,
-    max: 5,
-  },
-  logQueryParameters: NODE_ENV === "development",
-  benchmark: true,
-});
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    dialect: config.dialect,
+    host: config.host,
+    port: config.port,
+    timezone: "+07:00",
+    define: {
+      charset: "utf8mb4",
+      collate: "utf8mb4_general_ci",
+      underscored: true,
+      freezeTableName: true
+    },
+    pool: {
+      min: 0,
+      max: 5
+    },
+    logQueryParameters: NODE_ENV === "development",
+    benchmark: true
+  }
+);
 
 sequelize.authenticate();
 
